613d27ff197a022dd018f8c51a5de5233ca40499
Ismael Pastor <ispaluan@gmail.com>
2025-03-13 12:31:33 +0100

    ejercicio finalizado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
- // Aquí tu código
+ // Aquí tu código;
+
+ document.getElementById('agregar').addEventListener('click', function() {
+    let nuevoElemento = prompt('Añade un nuevo elemento');
+
+    if(nuevoElemento) {
+        let li = document.createElement('li');
+
+        li.textContent = nuevoElemento;
+
+        document.getElementById('lista').appendChild(li)
+    }
+ });
